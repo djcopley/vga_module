0ca33587bad687ddcb4eb843ea28a144152c6fed
Daniel Copley <djcopley@users.noreply.github.com>
2019-07-31 23:24:37 -0700

    fixed: pixel data now comes from outside block

--- test/vga_tb.vhd
+++ test/vga_tb.vhd
@@ -15,11 +15,18 @@ architecture rtl of vga_tb is
 
   constant RESET_PERIOD : time := 10 us;
 
+  constant WHITE_PIXEL : std_logic_vector(5 downto 0) := (others => '1');
+  alias R : std_logic_vector(1 downto 0) is WHITE_PIXEL(5 downto 4);
+  alias G : std_logic_vector(1 downto 0) is WHITE_PIXEL(3 downto 2);
+  alias B : std_logic_vector(1 downto 0) is WHITE_PIXEL(1 downto 0);
+
   signal clk : std_logic := '0';
   signal rst : std_logic := '0';
 
   signal hsync, vsync : std_logic;
+  signal pxl_rdy : std_logic;
 
+  signal i_red, i_blue, i_green : std_logic_vector(1 downto 0);
   signal o_red, o_blue, o_green : std_logic_vector(1 downto 0);
 
 begin
@@ -33,9 +40,11 @@ begin
         vsync => vsync,
 
         -- Input colors
-        i_red => (others => '0'),
-        i_blue => (others => '0'),
-        i_green => (others => '0'),
+        i_red => i_red,
+        i_blue => i_blue,
+        i_green => i_green,
+
+        pxl_rdy => pxl_rdy,
 
         -- Output colors
         -- 8 x 8 x 8 yields 512 different colors
@@ -57,5 +66,25 @@ begin
     wait;
   end process;
 
-  end architecture rtl;
+  stim : process
+  begin
 
+    wait until falling_edge(clk);
+    if pxl_rdy='1' then
+      i_red <= R;
+      i_green <= G;
+      i_blue <= B;
+    else
+      i_red <= (others => '0');
+      i_green <= (others => '0');
+      i_blue <= (others => '0');
+    end if;
+  end process;
+
+  kill_sim : process
+  begin
+    wait for 100 ms;
+    assert (false) report "-- SIMULATION COMPLETE --" severity failure;
+  end process;
+
+end architecture rtl;
